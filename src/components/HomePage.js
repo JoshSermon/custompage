1f48489075915f7a0f914f3083c626e83402f3a9
JJ Sermon <thejoshuasermon@gmail.com>
2021-09-18 22:28:52 -0400

    nav practice

---
+++ src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const HomePage = () => {
+    return (
+        <div>
+        <ul>
+           <li>
+              <>Home</>
+           </li>
+           <li>
+              <>Users</>
+           </li>
+           <li>
+              <>Contact</>
+           </li>
+        </ul>
+    </div>
+    )
+}
+
+export default HomePage
