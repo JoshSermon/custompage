6019eb2749416c421cdc463e61948b6d631c8ab1
JJ Sermon <thejoshuasermon@gmail.com>
2021-09-23 22:59:09 -0400

    navbar boot

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import HomePage from './components/HomePage';
+import Navbar from './components/Navbar';
 import logo from './logo.svg';
 import './App.css';
 import "./styles.css";
+import './components/Navbar';
+
 
 const App = () => {
   return (
     <div className="App">
-      <HomePage />
-
+      <Navbar />
     </div>
   );
 };
